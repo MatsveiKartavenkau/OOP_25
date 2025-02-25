203e302b01233819dcc89644f4f3dc28b7ab19b3
Matsvei <mkartavenkov@bk.ru>
2025-02-25 16:43:24 +0300

    +

--- svg/src/Point.java
+++ svg/src/Point.java
@@ -1,4 +1,26 @@
 public class Point {
     public double x;
     public double y;
+
+    public String toString() {
+        return "("+x+", "+y+")";
+    }
+
+    public String toSvg() {
+        return "<circle r=\"5\" cx=\""+x+"\" cy=\""+y+"\" fill=\"black\" />";
+    }
+    public void translate(double dx, double dy)
+    {
+        this.x += dx;
+        y += dy;
+    }
+    public Point translated(double dx, double dy)
+    {
+        Point newPoint = new Point();
+        newPoint.x = x+dx;
+        newPoint.y = y+dy;
+        return newPoint;
+    }
+
+
 }
