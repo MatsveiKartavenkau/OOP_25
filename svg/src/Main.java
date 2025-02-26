d524013f85578ddb98541e46181bbeb318911180
MacBook <macbook@MacBook-Air-MacBook.local>
2025-02-26 08:38:00 +0100

    Добавлен метод toSvgLine

--- svg/src/Main.java
+++ svg/src/Main.java
@@ -47,7 +47,7 @@ public class Main
         if (arr.length == 0)
             return null;
         Segment max = arr[0];
-        for( int i=0; i<arr.length; i++)
+        for ( int i=0; i<arr.length; i++)
         {
            if (arr[i].length() > max.length())
            {
--- svg/src/Point.java
+++ svg/src/Point.java
@@ -1,15 +1,29 @@
-public class Point
-{
+public class Point {
     public double x;
     public double y;
 
     public String toString() {
-        return "("+x+", "+y+")";
+        return "(" + x + ", " + y + ")";
     }
 
     public String toSvg() {
-        return "<circle r=\"5\" cx=\""+x+"\" cy=\""+y+"\" fill=\"black\" />";
+        return "<circle r=\"5\" cx=\"" + x + "\" cy=\"" + y + "\" fill=\"black\" />";
     }
+
+    public String toStringLine()
+    {
+        return "(" + x + ", " + y + ")";
+    }
+
+    public String toSvgLine()
+    {
+        return "<line x1=\"0\" y1=\"0\" x2=\"150\" y2=\"50\" stroke=\"blue\" stroke-width=\"5\" />";
+    }
+
+
+
+
+
     public void translate(double dx, double dy)
     {
         this.x += dx;
